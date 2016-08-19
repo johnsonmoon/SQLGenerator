daa2f68473a40e296e3d9d5793cb2550778a7cb7
xuyh@broada.com <xuyh@uyunsoft.cn>
2016-08-19 17:54:27 +0800

    添加代码，需要注意非字串值与字串值之间的区别(单引号)

--- src/main/java/Utils/SQLUtils.java
+++ src/main/java/Utils/SQLUtils.java
@@ -1,7 +1,41 @@
 package Utils;
 
+import java.util.List;
+import java.util.Map;
+
 /**
+ * 生成 JDBC SQL 语句的工具类
+ *
  * Created by Xuyh at 2016/08/19 下午 01:13.
  */
 public class SQLUtils {
+    /**
+     * 生成插入语句，注意value的顺序，顺序错误会导致数据插入错误
+     *
+     * @param tableName 表名
+     * @param values 值列表
+     * @return sql语句
+     */
+    public static String insertSentense(String tableName, List<Object> values){
+        String sql = "insert into ";
+        sql += "values(";
+        if(values.size() == 1){
+            sql += values.get(0);
+        }else{
+            sql += values.get(0);
+            for(int i = 1; i < values.size(); i++){
+                sql += (", " + values.get(i));
+            }
+        }
+        sql += ")";
+        return sql;
+    }
+
+    // delete from Accounts where Acc_ID = 'aa' and
+
+    public static String deleteSentense(String tableName, Map<String, Object> where){
+        String sql = "delete from " + tableName + " where ";
+
+        return sql;
+    }
 }
--- src/test/java/Utils/UtilsTest.java
+++ src/test/java/Utils/UtilsTest.java
@@ -1,7 +1,24 @@
 package Utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Xuyh at 2016/08/19 下午 01:15.
  */
 public class UtilsTest {
+    public static void main(String args[]){
+        testInsertSentense();
+    }
+
+    public static void testInsertSentense(){
+        String tableName = "Accounts";
+        List<String> values = new ArrayList<String>();
+        values.add("123456");
+        System.out.println(SQLUtils.insertSentense(tableName, values));
+        values.add("321564");
+        values.add("456789");
+        values.add("987654");
+        System.out.println(SQLUtils.insertSentense(tableName, values));
+    }
 }
