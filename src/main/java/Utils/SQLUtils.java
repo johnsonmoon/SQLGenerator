941930e2249c619be5a110067321f31d8585f176
xuyh@broada.com <xuyh@uyunsoft.cn>
2016-08-19 13:17:34 +0800

    First commit

---
+++ src/main/java/Utils/SQLUtils.java
@@ -0,0 +1,7 @@
+package Utils;
+
+/**
+ * Created by Xuyh at 2016/08/19 下午 01:13.
+ */
+public class SQLUtils {
+}
---
+++ src/test/java/Utils/UtilsTest.java
@@ -0,0 +1,7 @@
+package Utils;
+
+/**
+ * Created by Xuyh at 2016/08/19 下午 01:15.
+ */
+public class UtilsTest {
+}
